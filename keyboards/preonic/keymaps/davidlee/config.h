0cfde6a810577be9d070e93a2bad27ee82d5f07c
David Lee <dav@davlee.com>
2021-05-08 15:25:33 +1000

    get mouse emulation working

--- keyboards/preonic/keymaps/davidlee/config.h
+++ keyboards/preonic/keymaps/davidlee/config.h
@@ -32,6 +32,31 @@
 // allow leader
 #define LEADER_TIMEOUT 300
 #define LEADER_PER_KEY_TIMING
+
+
+// mouse stuff
+#define MK_KINETIC_SPEED                                
+
+#define MOUSEKEY_DELAY 8
+#define MOUSEKEY_INTERVAL 8
+#define MOUSEKEY_MOVE_DELTA 2    
+#define MOUSEKEY_INITIAL_SPEED 4
+#define MOUSEKEY_BASE_SPEED 1
+#define MOUSEKEY_DECELERATED_SPEED 1                        
+#define MOUSEKEY_ACCELERATED_SPEED 500
+
+#define MOUSEKEY_WHEEL_INITIAL_MOVEMENTS 16
+#define MOUSEKEY_WHEEL_BASE_MOVEMENTS 32                                
+#define MOUSEKEY_WHEEL_ACCELERATED_MOVEMENTS 48
+#define MOUSEKEY_WHEEL_DECELERATED_MOVEMENTS 8
+// #define MOUSEKEY_TIME_TO_MAX 100
+// #define MOUSEKEY_MAX_SPEED 20
+// #define MOUSEKEY_WHEEL_DELAY 20
+// #define MOUSEKEY_WHEEL_INTERVAL 40                                
+// #define MOUSEKEY_WHEEL_MAX_SPEED 2
+// #define MOUSEKEY_WHEEL_TIME_TO_MAX 100                                
+// // #define MK_COMBINED
+// #define MK_3_SPEED 75                             
                             
 /*
  * MIDI options
