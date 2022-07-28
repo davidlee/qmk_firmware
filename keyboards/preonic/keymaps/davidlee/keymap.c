475bec1e24c3190e8343bc03343d87d4df0a265e
David Lee <dav@davlee.com>
2022-07-28 15:31:17 +1000

    experiments w/ symbol placement

--- keyboards/preonic/keymaps/davidlee/keymap.c
+++ keyboards/preonic/keymaps/davidlee/keymap.c
@@ -124,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  Q_HYP,   W_MEH,   KC_F,    P_PAD,   KC_B,    KC_J,    KC_L,    KC_U,    Y_MEH,   SCLN_HYP,KC_DEL,
   ESC_CMD, A_CTRL,  R_OPT,   S_CMD,   T_SHIFT, KC_G,    KC_M,    N_SHIFT, E_CMD,   I_OPT,   O_CTRL,  CMD_QOT,
   FN_SFT,  KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, ENT_SFT,
-  PTR,     KC_LCTL, KC_LOPT, CMD_TAB, SPC_NUM, MIN_FUN, E_NAV,   BS_SFT,  KC_ENT,  FN,      SIRI,    MSN_CTL // PREV_DT, NEXT_DT, MSN_CTL
+  PTR,     KC_LCTL, KC_LOPT, CMD_TAB, SPC_NUM, MIN_FUN, E_NAV,   BS_SFT,  KC_ENT,  KC_F20,  SIRI,    FN
 ),
 
 [_TYP] = LAYOUT_preonic_grid(
@@ -147,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_PLUS, KC_COLN, KC_BSPC,
   CAP_WRD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC,
-  KC_CAPS, EN_DASH, EM_DASH, KC_MINS, KC_EQL,  KC_UNDS, KC_GRV,  KC_BSLS, KC_COMM, KC_DOT,  KC_SLSH, _______,
+  KC_CAPS, EM_DASH, KC_MINS, KC_EQL,  KC_UNDS, KC_EXLM, KC_GRV,  KC_BSLS, KC_COMM, KC_DOT,  KC_SLSH, _______,
   _______, _______, _______, PAD_LCK, _______, KC_SPC,  XXXXXXX, _______, _______, _______, _______, _______
 ),
 
