e6a4be0aef8d64d1d5a494fdff4c0fee563879f3
David Lee <dav@davlee.com>
2022-08-12 11:34:47 +1000

    some reconceptualizin' layers on the planck. Time to check if typing is adversely affected.

--- keyboards/planck/keymaps/davidlee/keymap.c
+++ keyboards/planck/keymaps/davidlee/keymap.c
@@ -1,12 +1,14 @@
 #include QMK_KEYBOARD_H
 
 enum planck_layers {
-  _CMK,  // Colemak-DH
-  _MOD,  // HOME ROW MODS layer
-  _GAM,  // Gaming / QWERTY
-  _NAV,  // Navigation
-  _NUM,  // Numbers 
-  _PTR,  // Pointer
+  _CMK,   // Colemak-DH
+  _NAV,   // Navigation
+  _NUM,   // Numbers 
+  _FUN,   // Function keys
+  _GAM,   // Gaming / QWERTY
+  _PTR,   // Pointer
+  _MOD_L, // Modifiers Left
+  _MOD_R, // Modifiers Right
 };
 
 enum planck_keycodes {
@@ -25,30 +27,29 @@ enum planck_keycodes {
 #define FN       KC_F24
 #define SIRI     KC_F21
 
-// #define XXXXXXX  KC_NO
-
 // alpha mod / layer taps
 #define Z_MEH   MT(MOD_MEH, KC_Z)
 #define SLS_MEH MT(MOD_MEH, KC_SLSH)
 #define SCN_OPT MT(MOD_LALT, KC_SCOLON)
+#define Y_CMD   MT(MOD_RGUI, KC_Y)
+#define D_MOD   LT(_MOD_L, KC_D)
+#define H_MOD   LT(_MOD_R, KC_H)
 
-// left side mods
+// left side 
 #define ESC_CTL  LCTL_T(KC_ESCAPE)
 #define TAB_MEH MT(MOD_MEH, KC_TAB)
 
-// bottom row mods
+// bottom row 
 #define CMD_TAB  MT(MOD_LGUI, KC_TAB)
 #define SPC_NUM  LT(_NUM, KC_SPC)
-#define MOD      MO(_MOD) // MT(_MOD, KC_E)
 
 #define NAV      MO(_NAV) 
 #define BS_SFT   MT(MOD_LSFT, KC_BSPC)
 #define ENT_PTR  LT(_PTR, KC_ENTER)
 #define ENT_OPT  MT(MOD_LALT, KC_ENTER)
-#define MOD      MO(_MOD)
 
-// right side mods
-#define CMD_QOT  MT(MOD_RGUI, KC_QUOTE)
+// right side 
+#define QOT_CTL  MT(MOD_RCTL, KC_QUOTE)
 #define ENT_SFT  KC_SFTENT
 #define DEL_HYP  MT(MOD_HYPR, KC_DEL)
 
@@ -60,8 +61,7 @@ enum planck_keycodes {
 #define PASTE  LCMD(KC_V)
 
 // misc
-// #define EN_DASH LOPT(KC_MINUS)
-// #define EM_DASH LOPT(KC_UNDERSCORE)
+#define EM_DASH LOPT(KC_UNDERSCORE)
 
 #define CMD_LBRC LCMD(KC_LBRC)
 #define CMD_RBRC LCMD(KC_RBRC)
@@ -82,18 +82,20 @@ enum planck_keycodes {
 // Combos
 //
 
-// N,U,I combo turns on PTR layer
-const uint16_t PROGMEM ptr_combo[] = {KC_N, KC_U, KC_I, COMBO_END};
+// N,O combo turns on PTR layer
+const uint16_t PROGMEM ptr_combo[] = {KC_N, KC_O, COMBO_END};
 // N,S is backspace word
 const uint16_t PROGMEM bs_word_combo[] = {KC_N, KC_E, COMBO_END};
 // A,R is caps word
 const uint16_t PROGMEM caps_word_combo[] = {KC_A, KC_R, COMBO_END};
-
+// ESC, DEL is RESET
+const uint16_t PROGMEM reset_combo[] = {ESC_CTL, DEL_HYP, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   COMBO(ptr_combo,        PTR_LCK),
   COMBO(bs_word_combo,    BS_WORD),
   COMBO(caps_word_combo,  CAP_WRD),
+  COMBO(reset_combo,      RESET),
 };
 
 //
@@ -104,34 +106,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_CMK] = LAYOUT_planck_grid(
   // KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    BS_WORD, 
-  TAB_MEH, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    SCN_OPT, DEL_HYP,
-  ESC_CTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    CMD_QOT,
-  KC_LSFT, Z_MEH,   KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  SLS_MEH, ENT_OPT,
-  FN,      KC_LCTL, KC_LOPT, CMD_TAB, SPC_NUM, MOD,     NAV,     BS_SFT,  ENT_PTR, _______, _______, SIRI
+  TAB_MEH, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    Y_CMD,   SCN_OPT, DEL_HYP,
+  ESC_CTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    QOT_CTL,
+  KC_LSFT, Z_MEH,   KC_X,    KC_C,    D_MOD,   KC_V,    KC_K,    H_MOD,   KC_COMM, KC_DOT,  SLS_MEH, ENT_OPT,
+  FN,      KC_LCTL, KC_LOPT, CMD_TAB, SPC_NUM, CMD_TAB, NAV,     BS_SFT,  ENT_PTR, _______, _______, _______
 ),
 
 [_NAV] = LAYOUT_planck_grid(
   // _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  KC_TAB,  KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MUTE, XXXXXXX, CMD_LBRC,CMD_MINS,CMD_EQL ,CMD_RBRC,_______,
+  KC_TAB,  KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MUTE, XXXXXXX, CMD_LBRC,CMD_MINS,CMD_EQL, CMD_RBRC,KC_WH_U,
   _______, KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, XXXXXXX, KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_DEL,
-  KC_CAPS, UNDO,    CUT,     COPY,    KC_MPLY, PASTE,   XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
+  KC_CAPS, UNDO,    CUT,     COPY,    KC_MPLY, PASTE,   XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_WH_D,
   _______, _______, _______, KC_TAB,  _______, XXXXXXX, XXXXXXX, _______, XXXXXXX, _______, _______, _______
 ),
 
 [_NUM] = LAYOUT_planck_grid(
   // _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  KC_GRV,  KC_COLN, KC_LBRC, KC_RBRC, KC_UNDS, KC_BSLS, KC_SLSH, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_PERC, 
-  SIRI,    KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, KC_LPRN, KC_RPRN, KC_4,    KC_5,    KC_6,    KC_0,    KC_PLUS,
-  _______, KC_EXLM, KC_AT,   KC_HASH, KC_MINS, KC_EQL,  KC_COMM, KC_1,    KC_2,    KC_3,    KC_AMPR, _______,
-  _______, _______, _______, _______, _______, KC_CIRC, KC_DOT,  _______, KC_0,    KC_DLR,  XXXXXXX, XXXXXXX
+  KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_UNDS, KC_LPRN, KC_RPRN, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_AMPR, 
+  SIRI,    KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, KC_LBRC, KC_RBRC, KC_4,    KC_5,    KC_6,    KC_0,    KC_PLUS,
+  _______, KC_BSLS, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  KC_COLN, KC_1,    KC_2,    KC_3,    KC_SLSH, KC_ENT,
+  _______, _______, _______, EM_DASH, _______, _______, KC_0,    _______, KC_DOT,  _______, _______, _______
 ),
 
 [_PTR] = LAYOUT_planck_grid(
-  // _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-  XXXXXXX, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, RESET,
-  EXT_PTR, KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, GAM_LCK, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN2,
-  KC_LSFT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BTN3,
-  _______, _______, KC_BTN2, KC_BTN1, KC_BTN3, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX
+  _______, _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, EXT_PTR,
+  _______, KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, _______, KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN2,
+  _______, _______, _______, _______, _______, _______, _______, KC_BTN2, KC_BTN3, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, KC_BTN2, KC_BTN1, KC_BTN3, _______, _______, _______
+),
+
+[_FUN] = LAYOUT_planck_grid(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+  _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,   KC_F18, KC_F19,  KC_F20,  _______,
+  _______, _______, _______, GAM_LCK, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 [_GAM] = LAYOUT_planck_grid(
@@ -142,11 +150,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  _______, EXT_GAM, KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
-[_MOD] = LAYOUT_planck_grid(
-  // _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
-  FN,      KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, XXXXXXX, XXXXXXX, KC_RSFT, KC_RCMD, KC_LOPT, KC_RCTL, FN,
-  _______, UNDO,    CUT,     COPY,    XXXXXXX, PASTE,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+[_MOD_L] = LAYOUT_planck_grid(
+  _______, _______, PASTE,   FN,      _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, _______, _______, _______, _______, _______, _______, _______,
+  _______, UNDO,    CUT,     COPY,    _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+[_MOD_R] = LAYOUT_planck_grid(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_RSFT, KC_RCMD, KC_LOPT, KC_RCTL, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
@@ -158,13 +171,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case Z_MEH:
-        case SCN_OPT:
-        case SLS_MEH:
-          return false; // Do not select the hold action when another key is tapped.
-        default:
-          return true; // Immediately select the hold action when another key is tapped.
-        }
+      case Z_MEH:
+      case SCN_OPT:
+      case Y_CMD:
+      case SLS_MEH:
+      case D_MOD:
+      case H_MOD:
+        return false; // Do not select the hold action when another key is tapped.
+      default:
+        return true; // Immediately select the hold action when another key is tapped.
+      }
 }
 
 
@@ -193,7 +209,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
       
       break;
 
-    case _MOD:
+    case _MOD_L:
+    case _MOD_R:
       rgblight_setrgb (0xFF,  0x33, 0xFF);
       #ifdef AUDIO_ENABLE
         PLAY_SONG(layer_song);
