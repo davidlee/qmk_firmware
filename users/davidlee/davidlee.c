3bd680f5bec3baeffa34a50ffa326afb62a09a9f
David Lee <dav@davlee.com>
2022-08-26 19:44:12 +1000

    mostly refactoring / doc edits, and a little twiddling sounds

--- users/davidlee/davidlee.c
+++ users/davidlee/davidlee.c
@@ -2,7 +2,7 @@
 
 enum custom_layers {
   _CMK,   // Colemak-DH
-  _ATH,   // Home Row Mods / alpha hold-taps
+  _HRM,   // Home Row Mods / alpha hold-taps
   _NAV,   // Navigation
   _NUM,   // Numbers 
   _FUN,   // Function keys
@@ -19,8 +19,8 @@ enum custom_keycodes {
   EXT_PTR,
   EXT_GAM,
   BS_WORD,
-  ATH_ON,  // ALPHA TAP / HOLD
-  ATH_OFF,
+  HRM_ON,  // ALPHA TAP / HOLD
+  HRM_OFF,
 };
 
 //
@@ -87,11 +87,13 @@ enum custom_keycodes {
 
 #ifdef AUDIO_ENABLE
   #define BLIP  SD_NOTE(_C0), SD_NOTE(_C1), TD_NOTE(_C3)
+  #define ARP   ED_NOTE(_C1), ED_NOTE(_DS1), ED_NOTE(_G1),  ED_NOTE(_C2), ED_NOTE(_DS2), ED_NOTE(_G2),  ED_NOTE(_C3), ED_NOTE(_G3), ED_NOTE(_C3), ED_NOTE(_G2), ED_NOTE(_DS2), ED_NOTE(_C2)
 
-  float layer_song[][2] = SONG(BLIP);
+  float blip_song[][2] = SONG(BLIP);
+  float arp_song[][2] = SONG(ARP);
 #endif
 
-#define LAYER_MASK_DEFAULT (1 << _CMK | 1 << _ATH)
+#define LAYER_MASK_DEFAULT (1 << _CMK | 1 << _HRM)
 
 //
 // Combos
@@ -125,9 +127,9 @@ const uint16_t PROGMEM combo_caps_word2[]= {KC_A, KC_R,   COMBO_END};
 const uint16_t PROGMEM combo_ptr[]       = {N_SFT, KC_U, COMBO_END};
 const uint16_t PROGMEM combo_ptr2[]      = {KC_N,  KC_U, COMBO_END};
 // U + Y = Turn ON Alpha Tap/Hold layer
-const uint16_t PROGMEM combo_ath_on[ ]   = {KC_U,  KC_Y,  COMBO_END};
+const uint16_t PROGMEM combo_hrm_on[ ]   = {KC_U,  KC_Y,  COMBO_END};
 // , + . = Turn OFF Alpha Tap/Hold layer
-const uint16_t PROGMEM combo_ath_off[]   = {COM_CMD, DOT_OPT, COMBO_END};
+const uint16_t PROGMEM combo_hrm_off[]   = {COM_CMD, DOT_OPT, COMBO_END};
 
 // ESC + DEL = RESET
 const uint16_t PROGMEM combo_reset[]     = {ESC_CTL, DEL_MEH, COMBO_END};
@@ -150,8 +152,8 @@ combo_t key_combos[COMBO_COUNT] = {
 
   COMBO(combo_ptr,        PTR_LCK),
   COMBO(combo_ptr2,       PTR_LCK),
-  COMBO(combo_ath_on,     ATH_ON),
-  COMBO(combo_ath_off,    ATH_OFF),
+  COMBO(combo_hrm_on,     HRM_ON),
+  COMBO(combo_hrm_off,    HRM_OFF),
 
   COMBO(combo_reset,      RESET),
 };
@@ -160,29 +162,21 @@ combo_t key_combos[COMBO_COUNT] = {
 // Function Overrides
 //
 
+const int home_row_mod_keys[] = { A_CTL, R_OPT, S_CMD, T_SFT, N_SFT, E_CMD, I_OPT, O_CTL};
+
 void keyboard_post_init_user(void) {
   default_layer_set(LAYER_MASK_DEFAULT); // COLEMAK & ALPHA TAP/HOLDs
 }
 
+// give precedence to tap rather than hold for home row mods, to avoid accidental mistypes
+// note this gives hold precedence for eg SLS_CTL, DOT_OPT, COM_CMD
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-
-      case A_CTL:
-      case R_OPT:
-      case S_CMD:
-      case T_SFT:
-      
-      case N_SFT:
-      case E_CMD:
-      case I_OPT:
-      case O_CTL:
-
-      case SLS_MEH:
-      case Z_MEH:
-        return false; // Do not select the hold action when another key is tapped.
-      default:
-        return true; // Immediately select the hold action when another key is tapped.
-      }
+  for(int i=0; i < sizeof(home_row_mod_keys) / sizeof(home_row_mod_keys[0]); i++) {
+    if(home_row_mod_keys[i] == keycode) {
+      return false; // Do not select the hold action when another key is tapped.
+    }
+  }
+  return true; // Immediately select the hold action when another key is tapped.
 }
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
@@ -193,10 +187,10 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
         return TAPPING_TERM + 40;
       case R_OPT:
       case I_OPT:
-        return TAPPING_TERM + 70;   
+        return TAPPING_TERM + 70; // lazy ring fingers are the biggest culprit for misfires
       case S_CMD:
       case E_CMD:
-        return TAPPING_TERM + 30;
+        return TAPPING_TERM + 40;
       case T_SFT:
       case N_SFT:
         return TAPPING_TERM + 30;
@@ -204,7 +198,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
       case SLS_CTL:
       case DOT_OPT:
       case COM_CMD:
-        return TAPPING_TERM + 40;
+        return TAPPING_TERM + 40; // doesn't matter as much due to permissive_hold
 
       default:
         return TAPPING_TERM; 
@@ -235,14 +229,10 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     
     case _MED:
       rgblight_setrgb (0x00,  0xAA, 0xFF);
+      break;
 
     case _GAM:
       rgblight_setrgb (0xFF,  0x00, 0x00);
-      
-      #ifdef AUDIO_ENABLE
-        PLAY_SONG(layer_song);
-      #endif
-      
       break;
 
     case _CMK:
@@ -266,20 +256,32 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case GAM_LCK:
       layer_on(_GAM);
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(arp_song);
+        #endif       
       return false;
 
     case PTR_LCK:
       layer_on(_PTR);
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(arp_song);
+        #endif       
       return false; 
 
     // EXIT LAYERS
 
     case EXT_GAM:
       layer_off(_GAM);
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(arp_song);
+        #endif             
       return false;    
 
     case EXT_PTR:
       layer_off(_PTR);
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(blip_song);
+        #endif             
       return false;
 
     // other functions
@@ -288,7 +290,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         caps_word_on();
         #ifdef AUDIO_ENABLE
-          PLAY_SONG(layer_song);
+          PLAY_SONG(blip_song);
         #endif
       }
       return false;    
@@ -299,19 +301,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
 
-    case ATH_ON:        
+    case HRM_ON:        
       if (record->event.pressed) {
         #ifdef AUDIO_ENABLE
-          PLAY_SONG(layer_song);
+          PLAY_SONG(blip_song);
         #endif        
         default_layer_set(LAYER_MASK_DEFAULT); // COLEMAK & HOME ROW MODS
       }
 
       return false;
-    case ATH_OFF:        
+    case HRM_OFF:        
       if (record->event.pressed) {
         #ifdef AUDIO_ENABLE
-          PLAY_SONG(layer_song);
+          PLAY_SONG(blip_song);
         #endif        
         default_layer_set(_CMK); // COLEMAK without alpha tap-hold shenanigans 
       }
