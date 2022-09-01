6198dc14ffe21c5fc70b70a5fab164fbf71e187b
David Lee <dav@davlee.com>
2022-09-01 13:07:30 +1000

    minus is more useful than bspc up there i guess

--- users/davidlee/keymaps.h
+++ users/davidlee/keymaps.h
@@ -9,7 +9,7 @@
 
 #define __BLANKS   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 
-#define __CMK_R0   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    BS_WORD
+#define __CMK_R0   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS
 #define __CMK_R1   TAB_MEH, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, DEL_MEH
 #define __CMK_R2   ESC_CTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    QOT_OPT
 #define __CMK_R3   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    COM_CMD, DOT_OPT, SLS_CTL, ENT_SFT
