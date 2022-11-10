87714a390bc1f1e35b6e1d47ddadc18bca8efa56
David Lee <dav@davlee.com>
2022-11-10 17:54:14 +1100

    twiddle the EXIT GAMING LAYER position

---
+++ keyboards/lily58/keymaps/davidlee/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 27
+#define RGBLIGHT_LIMIT_VAL 120
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
