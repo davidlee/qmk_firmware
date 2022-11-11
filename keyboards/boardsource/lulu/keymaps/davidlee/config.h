ab7cfbb70c83376e1fefa9be0c443b5b87fb4dd7
David Lee <dav@davlee.com>
2022-11-11 19:54:20 +1100

    tryna get layer state synced up for nice RGB state indication, per https://github.com/qmk/qmk_firmware/blob/master/docs/feature_split_keyboard.md

--- keyboards/boardsource/lulu/keymaps/davidlee/config.h
+++ keyboards/boardsource/lulu/keymaps/davidlee/config.h
@@ -3,3 +3,8 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 // #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200 // Timeout window in ms in which the double tap can occur.
 // #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+
+// #define USE_I2C
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+#define SPLIT_MODS_ENABLE
