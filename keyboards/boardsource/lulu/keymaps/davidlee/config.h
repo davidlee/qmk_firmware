13e42b24e3ebbc945d636987bb693729dcc4e8e0
David Lee <dav@davlee.com>
2022-11-12 21:29:07 +1100

    stuck on getting split RGB working. Suspect it's an issue with access to layer state, not display.

--- keyboards/boardsource/lulu/keymaps/davidlee/config.h
+++ keyboards/boardsource/lulu/keymaps/davidlee/config.h
@@ -2,9 +2,19 @@
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 // #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200 // Timeout window in ms in which the double tap can occur.
-// #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
 
-// #define USE_I2C
+// #define MASTER_LEFT
+
+#define SPLIT_TRANSPORT_MIRROR
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_LED_STATE_ENABLE
 #define SPLIT_MODS_ENABLE
+#define SPLIT_OLED_ENABLE
+#define SPLIT_WPM_ENABLE
+#define SPLIT_ST7565_ENABLE
+
+#define RGBLIGHT_SPLIT
+#define RGBLED_SPLIT { 35, 35 }
+
+// #define SPLIT_POINTING_ENABLE
