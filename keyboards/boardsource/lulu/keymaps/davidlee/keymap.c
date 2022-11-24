887c143819cda02e6c0a3a344e9e6dd65c702a91
David Lee <dav@davlee.com>
2022-11-25 06:37:52 +1100

    reconcile lulu keymap and docs. Fix some things. OSL seems to work with hax for now.

--- keyboards/boardsource/lulu/keymaps/davidlee/keymap.c
+++ keyboards/boardsource/lulu/keymaps/davidlee/keymap.c
@@ -5,7 +5,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_CMK] = LAYOUT(
 // COLEMAK
-KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    OS_ON,
+KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LEAD,
 TAB_HYP, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, DEL_HYP,
 ESC_MEH, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    QOT_MEH,
 OSL_FUN, Z_CTL,   X_OPT,   C_CMD,   KC_D,    KC_V,    XXXXXXX, XXXXXXX, KC_K,    KC_H,    COM_CMD, DOT_OPT, SLS_CTL, ENT_SFT,
@@ -22,9 +22,9 @@ _______, _______, _______, _______, _______, _______, _______, _______, _______,
 
   [_NAV] = LAYOUT(
 _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-KC_TAB,  _______, _______, _______, _______, _______,                   KC_WH_U, CMD_LBRC,CMD_MINS,CMD_EQL, CMD_RBRC,KC_WH_U,
+KC_TAB,  _______, _______, _______, _______, _______,                   KC_WH_U, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_WH_U,
 KC_ESC,  KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, _______,                   KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_DEL,
-KC_CAPS, UNDO,    CUT,     COPY,    PASTE,   PASTE,   _______, _______, KC_WH_D, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_WH_D,
+KC_CAPS, UNDO,    CUT,     COPY,    PASTE,   PASTE,   _______, _______, KC_WH_D, CMD_LBRC,CMD_MINS,CMD_EQL, CMD_RBRC,KC_WH_D,
                            _______, KC_TAB,  KC_SPC,  MO_PTR,  _______, _______, _______, _______
   ),
 
@@ -62,7 +62,7 @@ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, KC_N,   
 
   [_PTR] = LAYOUT(
 _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
-KC_TAB,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_WH_U, KC_BTN1, KC_BTN2, KC_BTN3, _______, KC_WH_U,
+KC_TAB,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_WH_U, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_WH_U,
 PTR_LCK, KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, OSL_FUN,                   KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN2,
 KC_CAPS, UNDO,    CUT,     COPY,    PASTE,   PASTE,   XXXXXXX, XXXXXXX, KC_WH_D, KC_BTN1, KC_BTN2, KC_BTN3, KC_F15,  KC_WH_D, // f15 = center cursor w/ karabiner
                            GAM_LCK, _______, KC_SPC,  _______, KC_BTN2, KC_BTN1, KC_BTN3, EXT_PTR
