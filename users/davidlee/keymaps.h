75760ffc5fc7a014556edf24bba3c93b65a4c854
David Lee <dav@davlee.com>
2023-12-09 22:36:59 +1100

    twiddles for preonic, long time since a flash

--- users/davidlee/keymaps.h
+++ users/davidlee/keymaps.h
@@ -48,8 +48,8 @@
 #define __MED_R4   _______, _______, _______, _______, MO_PTR,  _______, _______, _______, XXXXXXX, _______, _______, _______
 
 #define __GAM_R0   QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC
-#define __GAM_R1   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL
-#define __GAM_R2   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT
+#define __GAM_R1   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL
+#define __GAM_R2   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT
 #define __GAM_R3   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT
 #define __GAM_R4   EXT_GAM, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  KC_LCTL, EXT_GAM, KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 
